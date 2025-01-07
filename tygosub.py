48855820c4b95e1a30824bc4d57f094575ed82b7
TY-dot-Q <Tygoberghuisbvw@gmail.com>
2025-01-07 14:54:42 +0100

    v1.01
    eerste aanpassing: grid is 3d (3 hoog), gates worden gezet op de middelste hoogte en een manier om te testen (werkt nog niet)

--- tygosub.py
+++ tygosub.py
@@ -1,38 +1,53 @@
 grid =[]
 max_z = 3 #diepte oftewel de layers
-max_y = 0 # verticale hoogte
-max_x = 0 # horizontale lengte
+gatenr=1
 
-
-def gridcreate (size) -> None:
-    global grid
-    rows, cols = size
-    layers = 3
-    grid = [[[0 for _ in range(cols)] for _ in range(rows)] for _ in range(layers)]
+def gridcreate (max_y, max_x) -> None:
+    global grid, max_z
+    grid = [[[0 for _ in range(max_y)] for _ in range(max_x)] for _ in range(max_z)]
+    print("grid succesfol gemaakt")
 
 def printgrid (max_z, max_y, max_x) -> None:
     global grid
     for z in range(max_z):
-        for y in range(max_y):
-            for x in range(max_x):
-                print(f"Value at grid[{z}][{y}][{x}] = {grid[z][y][x]}")
+        print(f"laag {z}")
+        for row in grid[z]:
+            print(row) 
+    print("klaar met printen")
 
-def addgate (x,y, gate_name) ->None:
-    global grid 
-    
-    grid[1][y][x]= gate_name
+def addgate (y,x) ->None:
+    global grid, gatenr
 
-    if y > max_y: #bepaald de grid hoogte en breedte
-        max_y = y
-    
-    if x > max_x:
-        max_x = x
+    grid[1][y][x] = gatenr #voeg de gate toe
 
-    print(f"gate met de naam {gate_name} toegevoegd op de coordinaten y={y}, x={x} ")
+    print(f"gate met het nummer {gatenr} toegevoegd op de coordinaten y={y}, x={x} ")
+    gatenr=gatenr+1
 
 def _init_() ->None:
     #vraag de gates op, tot dat de user "stop" typt, dan maak de grid en print het met de functies hiervboven
     global max_y, max_x
-    print("geef de cordinaten van de gates, typ: 'stop' wanneer je klaar bent")
-
     
+    user_hoogte = int(input("geeft de maximaale verticale hoogte: 'y'"))
+    max_y = user_hoogte
+
+    user_breedte = int(input("geeft de maximaale horizontale lengte: 'x'"))
+    max_x = user_breedte
+    
+    gridcreate(max_y, max_x)
+
+    print("geef de cordinaten van de gates, typ: 'stop' wanneer je klaar bent")
+    while True:
+        user_coordinates = input("coordinaten bijv: 'x,y'")
+        if user_coordinates.lower()=="stop":
+            break
+        
+        try:
+            y, x = map(int, user_coordinates.split(','))
+            addgate(y,x)
+        except ValueError:
+            print("eroor: ongeldige invoer")
+
+    print("de uiteindelijke grid is:")
+    printgrid()
+
+_init_()
