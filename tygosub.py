62c9931c789fba8968f8a6421d93115da0f968d2
TY-dot-Q <Tygoberghuisbvw@gmail.com>
2025-01-08 13:27:36 +0100

    v1.031
    classes toegevoegt, maakt het iets makkelijker te navigeren. (werkt als het goed is)

--- tygosub.py
+++ tygosub.py
@@ -1,85 +1,94 @@
-grid =[]
-gatenr=1
-max_z = 3#layers
+class gridedit:
+    def __init__(self, max_z):
+        """initaliseer de gridedit class"""
+        self.max_z=max_z
+        self.grid=[]
+        self.gatenr =1
 
-def gridcreate (max_y, max_x) -> None:
-    """
-    creeert een array 3d grid gebaseerde op de globale max_z en opgegeven max_y en x
-    let op dit vervangt alle waardes niet runnen nadat gates zijn opgegeven.
-    """
-    global grid, max_z
-    grid = [[[0 for _ in range(max_y)] for _ in range(max_x)] for _ in range(max_z)]
-    print("grid succesfol gemaakt")
+    def gridcreate (self, max_y, max_x) -> None:
+        """
+        creeert een array 3d grid gebaseerde op de globale max_z en opgegeven max_y en x
+        let op dit vervangt alle waardes niet runnen nadat gates zijn opgegeven.
+        """
+        self.grid = [[[0 for _ in range(max_x)] for _ in range(max_y)] for _ in range(self.max_z)]
+        print("grid succesfol gemaakt")
 
-def printgrid () -> None:
-    """
-    print de huidig grid status met gates en verschillende lagen.
-    """
-    global grid
-    global max_z
-    for z in range(max_z):
-        print(f"laag {z}")
-        for row in grid[z]:
-            print(row) 
-    print("klaar met printen")
-
-def addgate (y, x) ->None:
-    """
-    vervangt de 0 waarde van de gridcreate met een nummer 1,2,3 etc en je kan hiermee dus gates toevoegen.
-    gebruik x en y coordinaten
-    """
-    global grid, gatenr
-
-    grid[1][y][x] = gatenr #voeg de gate toe
-
-    print(f"gate met het nummer {gatenr} toegevoegd op de coordinaten y={y}, x={x} ")
-    gatenr=gatenr+1
-
-def addwire (y, x, z) ->None:
-    """
-    vervangt de 0 waarde van de gridcreate met een wire, checkt ook of er niet al een gate is. 
-    """
-    global grid
-    if grid[z][y][x] ==0:
-        grid[z][y][x] = "+" #voeg de wire toe
-        print(f"wire toegevoegd op de coordinaten y={y}, x={x} z={z} ")
+    def addgate (self, y, x) ->None:
+        """
+        vervangt de 0 waarde van de gridcreate met een nummer 1,2,3 etc en je kan hiermee dus gates toevoegen.
+        gebruik x en y coordinaten
+        """
+        if self.grid[1][y][x] ==0:
+            self.grid[1][y][x] = self.gatenr #voeg de gate toe
+            print(f"gate met het nummer {self.gatenr} toegevoegd op de coordinaten y={y}, x={x} ")
+            self.gatenr +=1
+        else:
+            print(f"er staat al iets namelijk \"{self.grid[1][y][x]}\"")
+        
     
-    else:
-        print(f"er staat al iets namelijk: \"{grid[z][y][x]}\"")
-
-def addgaterequest()->None:
-    """
-    geeft een makkelijke manier om meerdere gates toe te voegen
-    herhaalt de addgatefunctie totdat de user stop typ. de input is y,x coordinaten
-    """
-    print("geef de cordinaten van de gates, typ: 'stop' wanneer je klaar bent")
-    while True:
-        user_coordinates = input("coordinaten bijv: 'y,x'")
-        if user_coordinates.lower()=="stop":
-            break
+    def addwire (self, y, x, z) ->None:
+        """
+        vervangt de 0 waarde van de gridcreate met een wire, checkt ook of er niet al een gate is. 
+        """
+        if self.grid[z][y][x] ==0:
+            self.grid[z][y][x] = "+" #voeg de wire toe
+            print(f"wire toegevoegd op de coordinaten y={y}, x={x} z={z} ")
         
-        try:
-            y, x = map(int, user_coordinates.split(','))
-            addgate(y,x)
-        except ValueError:
-            print("eroor: ongeldige invoer")      
+        else:
+            print(f"er staat al iets namelijk: \"{self.grid[z][y][x]}\"")
 
-def addwirerequest()->None:
-    """
-    geeft een makkelijke manier om meerdere gates toe te voegen
-    herhaalt de addgatefunctie totdat de user stop typ. de input is y,x coordinaten
-    """
-    print("geef de cordinaten van de wires, typ: 'stop' wanneer je klaar bent")
-    while True:
-        user_coordinates = input("coordinaten bijv: 'y,x,z'")
-        if user_coordinates.lower()=="stop":
-            break
-        
-        try:
-            y, x, z = map(int, user_coordinates.split(','))
-            addwire(y,x, z)
-        except ValueError:
-            print("eroor: ongeldige invoer")     
+class userinput:
+    def __init__(self, gridedit_obj):
+        self.gridedit=gridedit_obj
+
+    def addgaterequest(self)->None:
+        """
+        geeft een makkelijke manier om meerdere gates toe te voegen
+        herhaalt de addgatefunctie totdat de user stop typ. de input is y,x coordinaten
+        """
+        print("geef de cordinaten van de gates, typ: 'stop' wanneer je klaar bent")
+        while True:
+            user_coordinates = input("coordinaten bijv: 'y,x'")
+            if user_coordinates.lower()=="stop":
+                break
+            
+            try:
+                y, x = map(int, user_coordinates.split(','))
+                self.gridedit.addgate(y,x)
+            except ValueError:
+                print("eroor: ongeldige invoer")      
+
+    def addwirerequest(self)->None:
+        """
+        geeft een makkelijke manier om meerdere wires toe te voegen
+        herhaalt de addwire functie totdat de user stop typ. de input is y,x, z coordinaten
+        """
+        print("geef de cordinaten van de wires, typ: 'stop' wanneer je klaar bent")
+        while True:
+            user_coordinates = input("coordinaten bijv: 'y,x,z'")
+            if user_coordinates.lower()=="stop":
+                break
+            
+            try:
+                y, x, z = map(int, user_coordinates.split(','))
+                self.gridedit.addwire(y,x, z)
+            except ValueError:
+                print("eroor: ongeldige invoer")  
+
+
+class output:
+    def __init__(self, gridedit_obj):
+        self.gridedit=gridedit_obj
+
+    def printgrid (self) -> None:
+        """
+        print de huidig grid status met gates en verschillende lagen.
+        """
+        for z in range(self.gridedit.max_z):
+            print(f"laag {z}")
+            for row in self.gridedit.grid[z]:
+                print(row) 
+        print("klaar met printen")
 
 
 def _init_() ->None:
@@ -93,12 +102,16 @@ def _init_() ->None:
     user_breedte = int(input("geeft de maximaale horizontale lengte: 'x'"))
     max_x = user_breedte
     
-    gridcreate(max_y, max_x)
+    gridedit_obj = gridedit(max_z=3)
+    gridedit_obj.gridcreate(max_y, max_x)
 
-    addgaterequest()
-    addwirerequest()
 
-    print("de uiteindelijke grid is:")
-    printgrid()
+    userinput_obj = userinput(gridedit_obj)
+    userinput_obj.addgaterequest()
+    userinput_obj.addwirerequest()
+
+    output_obj = output(gridedit_obj)
+    print("De uiteindelijke grid is:")
+    output_obj.printgrid()
 
 _init_()
