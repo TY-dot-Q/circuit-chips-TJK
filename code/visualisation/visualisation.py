398b278fa9ac146eb6f53073b2f21f5a0f7984af
Joey Oehlers <joey@MBP-van-Joey-2.home>
2025-01-24 12:25:00 +0100

    voeg write-to-csv toe aan class output

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -108,3 +108,37 @@ class output:
 
         # Toon de animatie
         plt.show()   
+    
+    def write_to_csv(self, wirepaths_list):
+        # Open het CSV-bestand in 'append' mode
+        grid_edit_obj = self.grid_edit
+        with open('wirepaths.csv', 'a', newline='') as csvfile:
+            kolom = ['nummer', 'pad', 'succes', 'score', 'aantal_wires', 'aantal_kruizingen']
+            writer = csv.DictWriter(csvfile, fieldnames=kolom)
+
+            # Als het bestand leeg is, schrijf dan eerst de header (kolomnamen)
+            csvfile.seek(0, 2)  # Ga naar het einde van het bestand
+            if csvfile.tell() == 0:  # Als het bestand leeg is, schrijf header
+                writer.writeheader()
+
+            # Genereer nummer voor de nieuwe rij
+            with open('wirepaths.csv', 'r', newline='') as check_csvfile:
+                reader = csv.reader(check_csvfile)
+                rows = list(reader)
+                nummer = len(rows)  # Nummer is gelijk aan het aantal rijen, zodat het begint bij 1
+
+                # Dit zijn de andere gegevens die je wilt toevoegen. Pas deze aan op basis van je eigen logica.
+                data = {
+                    'nummer': nummer,
+                    'pad': str(wirepaths_list),  # Zet de wirepath om in een string
+                    'succes': 'ja',  # klopt niet - als er geen overlapping is dus twee twee 
+                    'score': 100,  # klopt niet
+                    'aantal_wires': grid_edit_obj.wirecount,  # Aantal draden in netwerk
+                    'aantal_kruizingen': grid_edit_obj.wirecrosscount  # aantal kruisingen 
+                }
+
+                # Schrijf in csv bestand
+                writer.writerow(data)
+                nummer += 1  # Verhoog het nummer voor de volgende rij
+
+        print("CSV-bestand succesvol geschreven.")
