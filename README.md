fd17cdc063675853823fca85f68246b4c5a8b9ac
Joey Oehlers <joey@sp-byods-145-109-14-46.wireless.uva.nl>
2025-01-31 17:48:26 +0100

    layout

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Chips & Circuits
 Groepsproject voor de minor Programmeren met het vak Algoritme & Heuristieken waarbij wij de beste manier onderzochten om in een chip alle gates uit de netlist op een efficiente manier met elkaar te verbinden in een grid van maximaal 8 verdiepingen hoog zonder draden te laten overlappen. Dit moet worden gedaan zonder dat er kruisingen van draden optreden. Indien er een kruising optreedt, wordt er extra kosten toegevoegd. De draden kunnen bewegen naar noord, zuid, oost west, maar ook omhoog of omlaag tussen verschillende lagen van de chip.
 
-# Team TJK - Programmeurs
+## Team TJK - Programmeurs
 * Tygo Berghuis
 * Keenan Kesser
 * Joey Oehlers
 
-### Nodig voor code
+# Nodig voor code
 De betreffende code is geschreven in Python 3. In requirements.txt staat alles wat noodzakelijk is om de code optimaal te runnen. Installatie is mogelijk met pip op de volgende manier: 
 """
 pip install -r requirements.txt
@@ -14,7 +14,7 @@ pip install -r requirements.txt
 
 Mocht onverhoopt de code niet draaien met deze installaties of kun je de installaties niet installeren probeer dan eerst een aparte omgeving te maken voor deze instellingen.
 
-### Structuur
+# Structuur
 In de mainfolder bevinden zich verschillende subfolders en bestanden:
 
 * main.py - Hiermee voer je het programma uit.
@@ -29,8 +29,26 @@ In de mainfolder bevinden zich verschillende subfolders en bestanden:
 * Scripts - alle scripts die gebruikt worden om het programma te laten draaien
 * Presentaties - alle netlists zoals gegeven in de opdracht
 
-### Programma draaien
+# Programma draaien
+**Hoe stel je de code in**
+In main.py staan drie verschillende categorieën in de code
+1. Setup
+2. Algoritmen
+3. Resultaten generen
 
+Setup en resultaten genereren zijn niet bedoelt om aan te passen!
+Dit staat ook duidelijk vermeld in de docstring
+
+Kies in main.py in categorie 2 (Algoritmen) het algoritme dat je wil runnen
+door de overige algoritme non-actief te maken met een docstring notatie zoals hieronder:
+
+"""
+Deze tekst staat in een docstring
+"""
+
+Zo runt enkel de code op het algoritme naar jouw keuze.
+
+**Hoe run je de code**
 Voer de volgende command in de terminal om het programma te laten draaien
 (Zorg dat je in de juiste map zit in de terminal)
 
@@ -38,17 +56,19 @@ Voer de volgende command in de terminal om het programma te laten draaien
 python3 main.py
 """
 
-In de terminal zal gevraagd worden naar het aantal iteraties
-`voer in hoeveel iteraties je wil doen.
+Voer eventuele vragen in de terminal uit en Klaar is Kees!
+
+**Hoe zit het met de output die gegenereert wordt**
 Het beste resultaat van de run wordt opgeslagen en weergegeven.
 alle iteraties worden bewaard in data.csv totdat je de code opnieuw runt.
 
 alle output die het programma produceert komt in de onderstaande map:
+
 """
 /data
 """
 
-# Uitleg algoritmes
+# Uitleg Algoritmes
 ## A*-pathfinding
 Het algoritme vindt een valide oplossing door over de netlist heen te loopen en voor iedere combinatie aan gates een pad te vinden. Dat pad vindt het door te starten bij de coördinaten van de eerste gate. Hij berekent dan voor de buurcoördinaten, met een met de Manhattan Distance heuristiek, de kosten om naar de coördinaten van de tweede gate te gaan. Hij doet de kosten en coördinaten van alle buren in een prioriteitslijst en gaat dan de goedkoopste optie als eerst af, terwijl hij bijhoudt waar hij vandaan komt. Dit wordt herhaald totdat hij bij het eindpunt is gekomen. Dan reconstrueert hij het pad terug naar het begin en maakt hij een wire aan en past dat aan in het grid. Dit wordt herhaald voor elke combinatie in de netlist. 
 
--- main.py
+++ main.py
@@ -10,22 +10,27 @@ from code.algorithms.crossless.hill_climber_nc import hil_climber_nc
 from code.algorithms.crossless.mh_nc import mh_nc
 
 if __name__ == "__main__":
-    #-----------------setup-------------------------------
-
+    #-----------------SETUP-------------------------------
+    """
+    Gelieve de setup niet te veranderen tenzij je de chips en/of netlist
+    wil veranderen!!!
+    Om werking van alle algoritmes te bewerkstellingen
+    """
     # maakt een instantie van grid edit aan en geeft dit door aan andere classes
     grid_edit_obj = grid_edit()
-
     user_input_obj = user_input(grid_edit_obj)
     output_obj = output(grid_edit_obj)
     start_obj = auto_functions(grid_edit_obj)
     algorithm_obj= MD(grid_edit_obj)
     netlist_reorder_obj=netlist_reordering(grid_edit_obj)
     hil_climber_obj=hil_climber(grid_edit_obj)
-
     hil_climber_nc_obj=hil_climber_nc(grid_edit_obj)
     mh_nc_obj=mh_nc(grid_edit_obj)
     
     # Paden van de grid en netlist die je wilt oplossen.
+    """
+    Verander hier de chip en/of netlist waarvan je de oplossing wil genereren
+    """
     grid_path="data/chip_1/print_1.csv"
     netlist_path="data/chip_1/netlist/netlist_1.csv"
 
@@ -38,35 +43,52 @@ if __name__ == "__main__":
     # maak de grid aan        
     start_obj.Auto_start_functie(grid_path)
 
-
-    #----------------Netlist passer------------------------
+    # netlist reorder heuristiek
     netlist_list=netlist_reorder_obj.netlist_reorder(netlist_path)
     netlist_list=user_input_obj.load_netlist(netlist_path)
     
     
-    #----------------No cross finder-----------------------
+    #----------------ALGORITMES----------------------------
+    #***************** No cross finder **********************
+    """
+    Dit algoritme gebruikt een hill climber die ten koste van alles
+    kruisingen voorkomt
+    """
+
+    """
     # hoeveel wires je per keer wilt laten verwijderen en opnieuw leggen
     reset_wires_amount = 3 #default = 3
 
     # de hoeveelheid tijd in minuten dat het opnieuw gaat lopen
-    reloop_time = 1 # default = 1
+    reloop_time = 30 # default = 30 
 
+    # A* algoritme met effectieve hillclimber algoritme
     mh_nc_obj.netlist_looper(netlist_list)
-    optimum=hil_climber_nc_obj.start_hill_climb(reset_wires_amount, netlist_list, reloop_time)
-    
-    #----------------Manhatten distance---------------------
-    algorithm_obj.netlist_looper(netlist_list)
+    optimum=hil_climber_nc_obj.start_hill_climb(reset_wires_amount, netlist_list, reloop_time)"""
 
 
-    #----------------Hill climber---------------------------
-    # reset_wires = 5 #hoeveel wires je per keer wilt laten verwijderen en opnieuw leggen
-    # reloop_time = 5 # de hoeveelheid tijd in minuten dat het opnieuw gaat lopen
-    #hil_climber_obj.start_hill_climb(reset_wires, netlist_list, reloop_time) #wertk nog niet
+    #********************* Hill climber *************************
+    """
+    Dit algoritme is een hill climber die wel kruisingen aangaat
+    Deze werkt nog niet optimaal
+    """
 
-    
-    # ----------------iteration runner----------------------------
-    """# Run the algorithm for a number of iterations
-    max_iterations = 100000
+    """
+    # hoeveel wires je per keer wilt laten verwijderen en opnieuw leggen
+    reset_wires = 3 # default = 3
+
+    # de hoeveelheid tijd in minuten dat het opnieuw gaat lopen
+    reloop_time = 30 # default = 30 
+    hil_climber_obj.start_hill_climb(reset_wires, netlist_list, reloop_time)"""
+
+    # ************* Manhattan Distance met iteration runner*********
+    """
+    Dit algoritme zoekt het kortste pad met aanvullende heuristieken
+    Dit algoritme legt maar een netwerk, dus voor beste oplossing moeten 
+    meerdere iteraties gedaan worden
+    """
+    # Run the algorithm for a number of iterations
+    max_iterations = 10000
     iterations = input("Hoeveel iteraties wil je runnen?: ")
     while not iterations.isdigit() or int(iterations) > max_iterations:
         iterations = input("Voer een geldig getal in tussen 1 en 100000: ")
@@ -75,7 +97,13 @@ if __name__ == "__main__":
         grid_edit_obj.reset_grid()  # Reset the grid while keeping the gates in place
 
         # Run the algorithm on the netlist
-        algorithm_obj.netlist_looper(netlist_list)"""
+        algorithm_obj.netlist_looper(netlist_list)
+
+    #---------------RESULTATEN GENEREREN----------------------
+    """
+    Zorgt dat alle parameters en het genereren van de output 
+    succesvol verlopen. Dus ook deze niet aanpassen!!!
+    """
 
     # berekent aantal draden en kruisingen
     wirecount = grid_edit_obj.update_wirecount()
@@ -86,9 +114,7 @@ if __name__ == "__main__":
     user_input_obj.match_wirepaths_to_nets(netlist_list)
     output_obj.write_to_csv(wirecount, name_file)
 
-
-
-    # Sla resultaten op in een csv en visualiseer
+    # Sla resultaten op in csv en visualiseer
     output_obj.load_best_result(name_file)
     match_wires = user_input_obj.match_wirepaths_to_nets(netlist_list)
     output_obj.output_to_csv(match_wires, netlist_path)
